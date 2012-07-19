83f984762b9074390d0c6c465bf15f48a4c7dfb7
Edmund Haselwanter <edmund@haselwanter.com>
2012-07-19 03:52:29 +0200

    Import database version 1.2.0

--- vagrant-java-application-reworked/cookbooks/database/recipes/ebs_volume.rb
+++ vagrant-java-application-reworked/cookbooks/database/recipes/ebs_volume.rb
@@ -31,7 +31,12 @@ if node[:ec2]
     raise
   end
 
-  ebs_vol_dev = node[:mysql][:ebs_vol_dev]
+  ebs_vol_dev = node['mysql']['ebs_vol_dev']
+  if (platform?("ubuntu") && node['platform_version'].to_f >= 11.04)
+    ebs_vol_dev_mount =  ebs_vol_dev.sub(/^\/dev\/sd/, "/dev/xvd")
+  else
+    ebs_vol_dev_mount = ebs_vol_dev
+  end
   ebs_vol_id = String.new
   db_type = String.new
   db_role = String.new
@@ -44,15 +49,15 @@ if node[:ec2]
     if (app["database_master_role"] & node.run_list.roles).length == 1 || (app["database_slave_role"] & node.run_list.roles).length == 1
       master_role = app["database_master_role"]
       slave_role = app["database_slave_role"]
-      root_pw = app["mysql_root_password"][node.app_environment]
-      snapshots_to_keep = app["snapshots_to_keep"][node.app_environment]
+      root_pw = app["mysql_root_password"][node.chef_environment]
+      snapshots_to_keep = app["snapshots_to_keep"][node.chef_environment]
 
       if (master_role & node.run_list.roles).length == 1
         db_type = "master"
-        db_role = master_role
+        db_role = RUBY_VERSION.to_f <= 1.8 ? master_role : master_role.join
       elsif (slave_role & node.run_list.roles).length == 1
         db_type = "slave"
-        db_role = slave_role
+        db_role = RUBY_VERSION.to_f <= 1.8 ? slave_role : slave_role.join
       end
 
       Chef::Log.info "database::ebs_volume - db_role: #{db_role} db_type: #{db_type}"
@@ -60,27 +65,27 @@ if node[:ec2]
   end
 
   begin
-    ebs_info = Chef::DataBagItem.load(:aws, "ebs_#{db_role}_#{node[:app_environment]}")
+    ebs_info = Chef::DataBagItem.load(:aws, "ebs_#{db_role}_#{node.chef_environment}")
     Chef::Log.info("Loaded #{ebs_info['volume_id']} from DataBagItem aws[#{ebs_info['id']}]")
   rescue
-    Chef::Log.warn("Could not find the 'ebs_#{db_role}_#{node[:app_environment]}' item in the 'aws' data bag")
+    Chef::Log.warn("Could not find the 'ebs_#{db_role}_#{node.chef_environment}' item in the 'aws' data bag")
     ebs_info = Hash.new
   end
 
   begin
-    master_info = Chef::DataBagItem.load(:aws, "ebs_#{master_role}_#{node.app_environment}")
+    master_info = Chef::DataBagItem.load(:aws, "ebs_#{master_role}_#{node.chef_environment}")
     Chef::Log.info "Loaded #{master_info['volume_id']} from DataBagItem aws[#{master_info['id']}]"
   rescue
     Chef::Application.fatal! "Could not load replication masters snapshot details", -41 if db_type == "slave"
   end
 
-  ruby_block "store_#{db_role}_#{node[:app_environment]}_volid" do
+  ruby_block "store_#{db_role}_#{node.chef_environment}_volid" do
     block do
-      ebs_vol_id = node[:aws][:ebs_volume]["#{db_role}_#{node[:app_environment]}"][:volume_id]
+      ebs_vol_id = node[:aws][:ebs_volume]["#{db_role}_#{node.chef_environment}"][:volume_id]
 
       unless ebs_info['volume_id']
         item = {
-          "id" => "ebs_#{db_role}_#{node[:app_environment]}",
+          "id" => "ebs_#{db_role}_#{node.chef_environment}",
           "volume_id" => ebs_vol_id
         }
         Chef::Log.info "Storing volume_id #{item.inspect}"
@@ -94,7 +99,7 @@ if node[:ec2]
     action :nothing
   end
 
-  aws_ebs_volume "#{db_role}_#{node[:app_environment]}" do
+  aws_ebs_volume "#{db_role}_#{node.chef_environment}" do
     aws_access_key aws['aws_access_key_id']
     aws_secret_access_key aws['aws_secret_access_key']
     size 50
@@ -111,7 +116,7 @@ if node[:ec2]
       else
         action [ :create, :attach ]
       end
-      notifies :create, resources(:ruby_block => "store_#{db_role}_#{node[:app_environment]}_volid")
+      notifies :create, resources(:ruby_block => "store_#{db_role}_#{node.chef_environment}_volid")
     when "slave"
       if master_info['volume_id']
         snapshot_id master_info['volume_id']
@@ -141,7 +146,7 @@ if node[:ec2]
           'db_snapshot' => {
             'ebs_vol_dev' => node.mysql.ec2_path,
             'db_role' => db_role,
-            'app_environment' => node.app_environment,
+            'app_environment' => node.chef_environment,
             'username' => 'root',
             'password' => root_pw,
             'aws_access_key_id' => aws['aws_access_key_id'],
@@ -171,24 +176,24 @@ if node[:ec2]
     end
   end
 
-    execute "mkfs.xfs #{ebs_vol_dev}" do
-      only_if "xfs_admin -l #{ebs_vol_dev} 2>&1 | grep -qx 'xfs_admin: #{ebs_vol_dev} is not a valid XFS filesystem (unexpected SB magic number 0x00000000)'"
+    execute "mkfs.xfs #{ebs_vol_dev_mount}" do
+      only_if "xfs_admin -l #{ebs_vol_dev_mount} 2>&1 | grep -qx 'xfs_admin: #{ebs_vol_dev_mount} is not a valid XFS filesystem (unexpected SB magic number 0x00000000)'"
     end
 
-  %w{ec2_path datadir}.each do |dir|
-    directory node[:mysql][dir] do
+  %w{ec2_path data_dir}.each do |dir|
+    directory node['mysql'][dir] do
       mode 0755
     end
   end
 
-  mount node[:mysql][:ec2_path] do
-    device ebs_vol_dev
+  mount node['mysql']['ec2_path'] do
+    device ebs_vol_dev_mount
     fstype "xfs"
     action :mount
   end
 
-  mount node[:mysql][:datadir] do
-    device node[:mysql][:ec2_path]
+  mount node['mysql']['data_dir'] do
+    device node['mysql']['ec2_path']
     fstype "none"
     options "bind,rw"
     action :mount
